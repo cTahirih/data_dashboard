4efb5bcbf622c67283dea6e4bc2bf15de6f4f4a0
cTahirih <tahysluana@gmail.com>
2018-09-22 11:33:35 -0500

    feat(project) init project with vue and pug

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  .dashboard
+    .header
+      nav.header__nav
+        ul
+          li  Cerrar sesión
+
+    .main
+      | First page
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: {
+    name: 'Rosa'
+  }
+}
+</script>
+
+<style>
+
+</style>
